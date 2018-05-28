92f84adef91d244dfdf9808b847d5c41dcef71f5
DevExpressExampleBot <fakeEmail>
2018-05-28 13:32:11 +0300

    Source auto update [skip ci]

---
+++ VB/HowToCreateFieldValueTemplate/CategoriesTableAdapter.vb
@@ -0,0 +1,13 @@
+﻿Namespace HowToCreateFieldValueTemplate
+
+
+    Partial Public Class CategoryPictures
+    End Class
+End Namespace
+
+Namespace HowToCreateFieldValueTemplate.CategoryPicturesTableAdapters
+
+
+    Partial Public Class CategoriesTableAdapter
+    End Class
+End Namespace
---
+++ VB/HowToCreateFieldValueTemplate/MainWindow.xaml.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Windows
+
+Namespace HowToCreateFieldValueTemplate
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Private salesPersonDataTable As New SalesPerson.SalesPersonDataTable()
+        Private salesPersonDataAdapter As New SalesPersonTableAdapters.SalesPersonTableAdapter()
+
+        Public Sub New()
+            InitializeComponent()
+            pivotGridControl1.DataSource = salesPersonDataTable
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub
+    End Class
+End Namespace
